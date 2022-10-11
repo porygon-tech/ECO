58ded26ba9367cc70596c93c3aa1c99766e46c1d
Roman <miguel.romansanchez@uzh.ch>
2022-10-11 15:13:08 +0200

    AUTO Di 11 Okt 2022 15:13:08 CEST

---
+++ dyn.py
@@ -0,0 +1,13 @@
+import numpy as np
+#import matplotlib.pyplot as plt
+#import networkx as nx
+from scipy import sparse
+from scipy.sparse import linalg
+#from scipy.optimize import minimize
+
+
+import threading
+import os
+from pathlib import Path
+import pickle5
+import bz2
